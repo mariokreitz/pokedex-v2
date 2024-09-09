de4d903bc8e126801f3852d3d372effdd5c8dc3a
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-09 16:30:40 +0200

    Create pokemon-overview.component.ts

---
+++ src/app/components/pokemon-overview/pokemon-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon-overview.component.html',
+  styleUrl: './pokemon-overview.component.scss'
+})
+export class PokemonOverviewComponent {
+
+}
