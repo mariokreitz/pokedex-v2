10b3c9510917bf5ce75813b20812c83e9d0daae8
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-09 13:28:22 +0200

    Create shared.service.ts

---
+++ src/app/services/shared.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SharedService {
+  private searchSubject = new BehaviorSubject<string>('');
+
+  currentSearch = this.searchSubject.asObservable();
+
+  constructor() {}
+
+  changeSearch(searchTerm: string) {
+    this.searchSubject.next(searchTerm);
+  }
+}
