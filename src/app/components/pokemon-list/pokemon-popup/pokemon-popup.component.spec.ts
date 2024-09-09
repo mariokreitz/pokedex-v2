dd25a357eeb189a00dfb558551f5c48aff9c380b
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-09 20:28:12 +0200

    Create pokemon-popup.component.spec.ts

---
+++ src/app/components/pokemon-list/pokemon-popup/pokemon-popup.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PokemonOverviewComponent } from './pokemon-popup.component';
+
+describe('PokemonOverviewComponent', () => {
+  let component: PokemonOverviewComponent;
+  let fixture: ComponentFixture<PokemonOverviewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PokemonOverviewComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PokemonOverviewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
