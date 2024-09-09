9cc9f807faabde547847fd6bbeb3a34f1ff378bc
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-09 02:00:43 +0200

    Create pokedex.ts

---
+++ src/types/pokedex.ts
@@ -0,0 +1,57 @@
+export type Pokemon = {
+  id: number;
+  name: string;
+  order: number;
+  types: {
+    slot: number;
+    type: {
+      name: string;
+      url: string;
+    };
+  }[];
+  stats: {
+    base_stat: number;
+    stat: {
+      name: string;
+      url: string;
+    };
+  }[];
+  height: number;
+  weight: number;
+  sprites: {
+    front_default: string | null;
+    other: {
+      dream_world: {
+        front_default: string | null;
+      };
+    };
+  };
+  abilities: {
+    ability: {
+      name: string;
+      url: string;
+    };
+    is_hidden: boolean;
+    slot: number;
+  }[];
+  game_indices: {
+    game_index: number;
+    version: {
+      name: string;
+      url: string;
+    };
+  }[];
+  held_items: {
+    item: {
+      name: string;
+      url: string;
+    };
+    version_details: {
+      rarity: number;
+      version: {
+        name: string;
+        url: string;
+      };
+    }[];
+  }[];
+};
