9c62644a94126ff3056b31f94977342b90eefb77
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-09 10:57:12 +0200

    Update pokemon-card.component.ts

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { Pokemon } from '../../../types/pokedex';
 import { DecimalPipe, TitleCasePipe } from '@angular/common';
+import { PokemonImageComponent } from '../pokemon-image/pokemon-image.component';
 
 @Component({
   selector: 'app-pokemon-card',
   standalone: true,
-  imports: [TitleCasePipe, DecimalPipe],
+  imports: [TitleCasePipe, DecimalPipe, PokemonImageComponent],
   templateUrl: './pokemon-card.component.html',
   styleUrl: './pokemon-card.component.scss',
 })
 export class PokemonCardComponent {
+  id!: number;
   name!: string;
   imgSrc!: string | null;
   types!: string[];
@@ -19,10 +21,15 @@ export class PokemonCardComponent {
   @Input()
   Pokemon!: Pokemon;
 
-  ngOnInit() {
-    this.name = this.Pokemon.name;
-    this.imgSrc = this.Pokemon.sprites.other.dream_world.front_default;
+  ngOnInit(): void {
+    const { id, name, sprites, types } = this.Pokemon;
 
-    this.types = this.Pokemon.types.map((pokemon) => pokemon.type.name);
+    this.id = id;
+    this.name = name;
+    this.imgSrc =
+      sprites.other.dream_world.front_default ??
+      sprites.other['official-artwork'].front_default;
+
+    this.types = types.map(({ type }) => type.name);
   }
 }
