c723b7495bd8ddec1fac55fc5f457ddee49a225e
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-10 12:21:43 +0200

    Update pokemon-popup.component.ts

--- src/app/components/pokemon-list/pokemon-popup/pokemon-popup.component.ts
+++ src/app/components/pokemon-list/pokemon-popup/pokemon-popup.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { Pokemon } from '../../../../types/pokedex';
 import { DecimalPipe, TitleCasePipe } from '@angular/common';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-pokemon-popup',
@@ -10,28 +11,17 @@ import { DecimalPipe, TitleCasePipe } from '@angular/common';
   styleUrl: './pokemon-popup.component.scss',
 })
 export class PokemonPopupComponent implements OnInit {
-  playCry() {
-    if (this.cries) {
-      if (this.cries.latest) {
-        const audio = new Audio(this.cries.latest);
-        audio.play();
-      } else {
-        const audio = new Audio(this.cries.legacy);
-        audio.play();
-      }
-    } else {
-      alert('Cry not found');
-    }
-  }
-  closePopup() {
-    document.getElementById('overview')?.classList.add('d_none');
-  }
   @Input() selectedPokemon: Pokemon | null = null;
 
   id!: number;
   name!: string;
   imgSrc!: string | null;
   types!: string[];
+  stats!: {
+    base_stat: number;
+    effort: number;
+    stat: { name: string; url: string };
+  }[];
   description!: string;
   cries?: {
     latest: string;
@@ -78,7 +68,80 @@ export class PokemonPopupComponent implements OnInit {
           ];
         this.description = randomDescription.flavor_text;
         this.cries = cries;
+        this.stats = stats;
+
+        this.populateStatsChart();
       }
     }
+  }
+
+  playCry() {
+    if (this.cries) {
+      if (this.cries.latest) {
+        const audio = new Audio(this.cries.latest);
+        audio.play();
+      } else {
+        const audio = new Audio(this.cries.legacy);
+        audio.play();
+      }
+    } else {
+      alert('Cry not found');
+    }
+  }
+  closePopup() {
+    document.getElementById('overview')?.classList.add('d_none');
+  }
+
+  populateStatsChart() {
+    const statNames = [
+      'HP',
+      'Attack',
+      'Defense',
+      'Sp. Atk',
+      'Sp. Def',
+      'Speed',
+    ];
+    const statData = this.stats.map(({ base_stat }) => base_stat);
+
+    const ctx = document.getElementById('stats-Chart') as HTMLCanvasElement;
+
+    let myChart = Chart.getChart(ctx);
+    if (myChart) {
+      myChart.destroy();
+    }
+    new Chart(ctx, {
+      type: 'radar',
+      data: {
+        labels: statNames,
+        datasets: [
+          {
+            label: 'Stats',
+            data: statData,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          r: {
+            ticks: {
+              display: false,
+              stepSize: 20,
+            },
+            pointLabels: {
+              color: 'white',
+            },
+            angleLines: {
+              display: true,
+              color: 'rgba(255, 255, 255, 0.75)',
+            },
+            grid: {
+              color: 'rgba(255, 255, 255, 0.75)',
+            },
+            suggestedMin: 0,
+            suggestedMax: 120,
+          },
+        },
+      },
+    });
   }
 }
