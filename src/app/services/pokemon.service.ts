21162a359aa15863c25ce4f57e87feb5ac7c3649
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-13 19:05:48 +0200

    refactor: rewrite pokemon.service and add JSDocs
    - Rewritten pokemon.service for improved structure and maintainability.
    - Added comprehensive JSDocs to document functions and enhance code readability.

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -2,43 +2,70 @@ import { Injectable } from '@angular/core';
 import { Pokedex, PokemonSpecies } from 'pokeapi-js-wrapper';
 import { Pokemon } from '../../types/pokedex';
 
+/**
+ * Provides methods to fetch Pokemon data from PokeAPI.
+ *
+ * @remarks
+ * This service is a wrapper around the PokeAPI JavaScript library.
+ * It provides methods to fetch a list of Pokemon, as well as individual
+ * Pokemon data. It also provides a method to fetch the species data of a
+ * Pokemon, which is used to fetch the Pokemon's types.
+ *
+ * @publicApi
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonService {
-  constructor() {}
-
-  customOptions = {
-    cache: true,
+  /**
+   * The PokeAPI client.
+   *
+   * @private
+   */
+  private readonly pokedex = new Pokedex({
+    protocol: 'https',
+    hostName: 'pokeapi.co',
+    versionPath: '/api/v2/',
     timeout: 5 * 1000,
-  };
+    cache: true,
+  });
 
-  pokedex = new Pokedex(this.customOptions);
-
+  /**
+   * The interval of Pokemon to fetch.
+   *
+   * @remarks
+   * The Pokemon are fetched in intervals of 151, starting from the
+   * beginning of the Pok dex.
+   *
+   * @private
+   */
   interval = {
     offset: 0,
     limit: 151,
   };
 
   /**
-   * Retrieves a list of Pokémon with their corresponding species and items.
+   * Fetches a list of Pokemon.
    *
-   * @return {Promise<Pokemon[]>} A promise that resolves to an array of Pokémon objects.
+   * @returns {Promise<Pokemon[]>} A promise that resolves with an array
+   * of Pokemon.
+   *
+   * @example
+   * const pokemons = await pokemonService.getPokemons();
+   * console.log(pokemons);
    */
   async getPokemons(): Promise<Pokemon[]> {
-    const listResponse = await this.pokedex.getPokemonsList(this.interval);
-    const pokemonNames = listResponse.results;
-
+    const response = await this.pokedex.getPokemonsList(this.interval);
+    const result = response.results;
     const pokemons = await Promise.all(
-      pokemonNames.map(async (pokemon) => {
+      result.map(async (pokemon) => {
         const species = await this.getPokemonSpeciesByNameWithFallback(
           pokemon.name
         );
-
         const data = await this.pokedex.getPokemonByName(pokemon.name);
-
-        const itemNames = data.held_items.map((item) => item.item.name);
-        const items = await this.pokedex.getItemByName(itemNames);
+        const items = await this.pokedex.getItemByName(
+          data.held_items.map((items) => items.item.name)
+        );
 
         return { ...data, ...species, items };
       })
@@ -48,23 +75,25 @@ export class PokemonService {
   }
 
   /**
-   * Retrieves a Pokémon species by name, falling back to the base name if the original name fails.
+   * Fetches the species data of a Pokemon.
    *
-   * @param {string} name - The name of the Pokémon species to retrieve.
-   * @return {Promise<PokemonSpecies>} A promise that resolves to the Pokémon species object.
+   * @param {string} name - The name of the Pokemon.
+   *
+   * @returns {Promise<PokemonSpecies>} A promise that resolves with the
+   * species data of the Pokemon. If the Pokemon is not found, it returns the
+   * species data of the Pokemon with the same name, but without the
+   * generation suffix.
+   *
+   * @private
    */
   private async getPokemonSpeciesByNameWithFallback(
     name: string
   ): Promise<PokemonSpecies> {
-    let species: PokemonSpecies;
-
     try {
-      species = await this.pokedex.getPokemonSpeciesByName(name);
+      return await this.pokedex.getPokemonSpeciesByName(name);
     } catch (error) {
       const baseName = name.split('-')[0];
-      species = await this.pokedex.getPokemonSpeciesByName(baseName);
+      return await this.pokedex.getPokemonSpeciesByName(baseName);
     }
-
-    return species;
   }
 }
