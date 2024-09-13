fdbd5cbcd1198889acaa60287c6fc95abfcc3a47
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-13 13:09:51 +0200

    docs: add JSDoc comments to ngOnInit method
    - Added JSDoc comments to the ngOnInit method for improved documentation and clarity on its purpose and behavior.

--- src/app/components/pokemon-card/pokemon-image/pokemon-image.component.ts
+++ src/app/components/pokemon-card/pokemon-image/pokemon-image.component.ts
@@ -13,6 +13,12 @@ export class PokemonImageComponent implements OnInit {
 
   imgSrc!: string;
 
+  /**
+   * Initializes the component after Angular has initialized all data-bound properties of the component.
+   * Sets the `imgSrc` property to the value of `src` if it is not null or undefined, otherwise sets it to an empty string.
+   *
+   * @return {void} This function does not return anything.
+   */
   ngOnInit(): void {
     this.imgSrc = this.src ?? '';
   }
