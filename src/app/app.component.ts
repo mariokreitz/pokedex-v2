cf0c01d3b34295035d3911c17a5b175305989445
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-15 19:07:23 +0200

    feat: add app-ui-settings component and adjust SCSS for seamless integration
    - Introduced app-ui-settings component for managing user interface preferences, including audio volume and other settings.
    - Tweaked SCSS to ensure the settings UI element fits seamlessly on the right side of the screen.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { HeaderComponent } from './components/header/header.component';
+import { UiSettingsComponent } from './components/ui-settings/ui-settings.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PokemonListComponent, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    PokemonListComponent,
+    HeaderComponent,
+    UiSettingsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
