2376821ff3ee762da8b60a14fe43fae93ac9771e
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-15 18:28:20 +0200

    Create ui-settings.component.ts

---
+++ src/app/components/ui-settings/ui-settings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-settings',
+  standalone: true,
+  imports: [],
+  templateUrl: './ui-settings.component.html',
+  styleUrl: './ui-settings.component.scss'
+})
+export class UiSettingsComponent {
+
+}
