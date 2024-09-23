e497507bd87444cdddf592af96db54e1a3defcb3
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-23 15:09:16 +0200

    feat: create boilerplate layout for imprint component
    - Established a basic layout structure for the imprint component to facilitate development.
    - Added placeholder content and styling for future enhancements.

--- src/app/components/imprint/imprint.component.ts
+++ src/app/components/imprint/imprint.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
   imports: [],
   templateUrl: './imprint.component.html',
-  styleUrl: './imprint.component.scss'
+  styleUrl: './imprint.component.scss',
 })
-export class ImprintComponent {
+export class ImprintComponent implements OnInit {
+  constructor(private settingsService: SettingsService) {}
 
+  get language(): string {
+    return this.settingsService.getLanguage();
+  }
+
+  ngOnInit(): void {}
 }
