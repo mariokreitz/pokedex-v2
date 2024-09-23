ec5289fa2e3d5441c20bd3f9e9915ce924053fe4
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-23 13:33:30 +0200

    feat: add imprint component to app.component
    - Integrated the newly created imprint component into the app.component.
    - Updated routing and display logic to include the imprint section.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { UiSettingsComponent } from './components/ui-settings/ui-settings.component';
 import { BackToTopComponent } from './components/back-to-top/back-to-top.component';
+import { ImprintComponent } from './components/imprint/imprint.component';
 
 /**
  * The root component of the application.
@@ -27,6 +28,7 @@ import { BackToTopComponent } from './components/back-to-top/back-to-top.compone
     HeaderComponent,
     UiSettingsComponent,
     BackToTopComponent,
+    ImprintComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
