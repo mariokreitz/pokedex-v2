7b59fb4467cf644d93910dbf42523ee27c7d928f
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-24 17:28:38 +0200

    refactor: update ngAfterViewInit and constructor, add handleKeydown()
    - Refactored ngAfterViewInit and constructor for improved structure and readability
    - Added handleKeydown() function to handle keyboard interactions
    - Added and updated JSDocs for better documentation and clarity

--- src/app/components/credits/credits.component.ts
+++ src/app/components/credits/credits.component.ts
@@ -7,6 +7,7 @@
  */
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../../services/settings.service';
+import { Router } from '@angular/router';
 
 /**
  * The component that displays the game's credits.
@@ -41,15 +42,30 @@ export class CreditsComponent implements OnInit {
   get language(): string {
     return this.settingsService.getLanguage();
   }
+
   /**
-   * Constructor
+   * Initializes the CreditsComponent by setting up the audio playback and event listeners.
    *
-   * Initializes the CreditsComponent with the necessary services.
-   *
-   * @param {SettingsService} settingsService - The settings service that provides the necessary data for this component.
+   * @param {SettingsService} settingsService - The settings service used to retrieve the current language.
+   * @param {Router} router - The router used to navigate to the root route when the audio ends.
    */
-  constructor(private settingsService: SettingsService) {
+  constructor(
+    private settingsService: SettingsService,
+    private router: Router
+  ) {
     this.audio = new Audio('./assets/game-music-loop-7-145285.mp3');
+
+    this.audio.addEventListener('ended', () => {
+      if (this.audio) {
+        this.audio.pause();
+        this.audio.currentTime = 0;
+        this.audio.volume = 0;
+      }
+      document.body.style.overflowY = 'unset';
+      this.router.navigate(['/']);
+    });
+
+    document.addEventListener('keydown', (event) => this.handleKeydown(event));
   }
 
   /**
@@ -63,7 +79,6 @@ export class CreditsComponent implements OnInit {
   ngOnInit(): void {
     if (this.audio) {
       let initialVolume = 0;
-      this.audio.loop = true;
       this.audio.volume = initialVolume;
       const intervalId = setInterval(() => {
         if (initialVolume < this.settingsService.getAudioVolume()) {
@@ -74,7 +89,6 @@ export class CreditsComponent implements OnInit {
         }
       }, 90);
       this.audio.play();
-      this.audio.loop = true;
     }
   }
 
@@ -94,18 +108,42 @@ export class CreditsComponent implements OnInit {
   }
 
   /**
-   *  Initializes the component's view after Angular has initialized the component's view and child views.
+   * Initializes the component's view after Angular has initialized the component's view and child views.
    *
-   *  @return {void} No return value.
+   * This function sets the opacity of the '.content' element to 0, hides the vertical scrollbar, and gradually
+   * increases the opacity to 1 over a duration of 3000ms using CSS transition. It also translates the '.content'
+   * element upwards by a distance equal to the window's inner height, and repeats this translation every 10ms.
+   *
+   * @return {void} No return value.
    */
   ngAfterViewInit(): void {
     const content = document.querySelector('.content') as HTMLDivElement;
     if (content) {
       content.style.opacity = '0';
+      document.body.style.overflowY = 'hidden';
       setTimeout(() => {
         content.style.transition = 'opacity 3000ms ease-in-out';
         content.style.opacity = '1';
       }, 125);
+
+      let translateY = window.innerHeight;
+      setInterval(() => {
+        translateY -= 1;
+        content.style.transform = `translateY(${translateY}px)`;
+      }, 10);
+    }
+  }
+
+  /**
+   * Handles keydown events to provide a way to navigate away from the current view.
+   *
+   * @param {KeyboardEvent} event - The keyboard event triggered by the user.
+   * @return {void} No return value.
+   */
+  handleKeydown(event: KeyboardEvent): void {
+    if (event.key === 'Escape') {
+      document.body.style.overflowY = 'unset';
+      this.router.navigate(['/']);
     }
   }
 }
