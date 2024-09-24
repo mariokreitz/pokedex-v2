d4178b1ac659c8e87b0c3cbe47b04410875e9a1a
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-24 10:08:40 +0200

    feat: create Credits component
    - Implemented new Credits component to display attribution and credits information

---
+++ src/app/components/credits/credits.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-credits',
+  standalone: true,
+  imports: [],
+  templateUrl: './credits.component.html',
+  styleUrl: './credits.component.scss'
+})
+export class CreditsComponent {
+
+}
