e4789cf94664ed829a30395615b2d4d963dd6ea7
Mario Kreitz <Mario.Kreitz@web.de>
2024-09-24 11:09:35 +0200

    style: update Credits page with basic styling and fade-in animation
    - Enhanced basic styling for the Credits page
    - Implemented fade-in animation for improved user experience
    - Updated content for English localization only

--- src/app/components/credits/credits.component.ts
+++ src/app/components/credits/credits.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-credits',
   standalone: true,
   imports: [],
   templateUrl: './credits.component.html',
-  styleUrl: './credits.component.scss'
+  styleUrl: './credits.component.scss',
 })
 export class CreditsComponent {
+  constructor(private settingsService: SettingsService) {}
 
+  /**
+   * Retrieves the currently set language from the settings service.
+   *
+   * @return {string} The currently set language.
+   */
+  get language(): string {
+    return this.settingsService.getLanguage();
+  }
+
+  /**
+   *  Initializes the component's view after Angular has initialized the component's view and child views.
+   *
+   *  @return {void} No return value.
+   */
+  ngAfterViewInit(): void {
+    const content = document.querySelector('.content') as HTMLDivElement;
+    if (content) {
+      content.style.opacity = '0';
+      setTimeout(() => {
+        content.style.transition = 'opacity 3000ms ease-in-out';
+        content.style.opacity = '1';
+      }, 125);
+    }
+  }
 }
